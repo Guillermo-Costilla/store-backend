677baa12d573652e2aeb29f9454688046013f033
Guillermo <gcostilla96@gmail.com>
2025-03-29 17:29:57 -0300

    env

--- config/config.js
+++ config/config.js
@@ -1,29 +1,26 @@
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Cargar .env desde la raíz del proyecto
-dotenv.config({ path: path.join(__dirname, '../.env') });
-
-export const STRIPE_PRIVATE_KEY = process.env.STRIPE_PRIVATE_KEY;
+dotenv.config({ path: join(__dirname, '../.env') });
 
 export const config = {
+    database: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+    },
     jwt: {
-        secret: process.env.JWT_SECRET
+        secret: process.env.JWT_SECRET,
     },
     stripe: {
-        privateKey: process.env.STRIPE_PRIVATE_KEY
-    },
-    database: {
-        host: 'sql10.freesqldatabase.com',
-        user: 'sql10770315',
-        password: 'Em4SAqG1pP',
-        database: 'sql10770315',
-        port: 3306
+        privateKey: process.env.STRIPE_PRIVATE_KEY,
     },
     server: {
         port: process.env.PORT || 5000
--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 import { config } from './config.js';
 
 const pool = mysql.createPool({
-    host: config.database.host, // 'sql10.freesqldatabase.com'
-    user: config.database.user, // 'sql10770315'
-    password: config.database.password, // 'Em4SAqG1pP'
-    database: config.database.database, // 'sql10770315'
-    port: config.database.port // 3306
+    host: config.database.host,
+    user: config.database.user, 
+    password: config.database.password,
+    database: config.database.database, 
+    port: config.database.port 
 });
 
 export default pool

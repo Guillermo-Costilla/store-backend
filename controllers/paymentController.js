10e77f0543b454e05cdf6078a48d10d32d442e3b
Guillermo <gcostilla96@gmail.com>
2025-04-05 23:12:43 -0300

    manejo de errores

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -24,9 +24,33 @@ export const paymentController = {
 
         } catch (error) {
             console.error('Error en el proceso de pago:', error);
+            if (error.type === 'StripeCardError') {
+                const charge = await stripe.charges.retrieve(error.payment_intent.latest_charge);
+                if (charge.outcome.type === 'blocked') {
+                    return res.status(400).json({ 
+                        success: false,
+                        error: 'Payment blocked for suspected fraud.' 
+                    });
+                } else if (error.code === 'card_declined') {
+                    return res.status(400).json({ 
+                        success: false,
+                        error: 'Payment declined by the issuer.' 
+                    });
+                } else if (error.code === 'expired_card') {
+                    return res.status(400).json({ 
+                        success: false,
+                        error: 'Card expired.' 
+                    });
+                } else {
+                    return res.status(400).json({ 
+                        success: false,
+                        error: 'Other card error.' 
+                    });
+                }
+            }
             res.status(400).json({ 
                 success: false,
-                error: error.message
+                error: error.message 
             });
         }
     }

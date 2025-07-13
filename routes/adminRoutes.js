b163335ff5b6522d88b2221b4f93bc057e5453f2
Guillermo Costilla <gcostilla96@gmail.com>
2025-07-12 22:22:06 -0300

    rutas

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,12 +4,12 @@ import {
   obtenerOrdenesCompletas, 
   obtenerOrdenDetallada 
 } from '../controllers/adminController.js';
-import { authenticateToken } from '../middlewares/authMiddleware.js';
+import { verifyAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Proteger todas las rutas de admin con autenticación
-router.use(authenticateToken);
+router.use(verifyAdmin);
 
 // Dashboard de métricas (solo admin)
 router.get('/dashboard', obtenerMétricasAdmin);

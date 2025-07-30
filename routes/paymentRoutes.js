1499393da33d0c8a03d1551d7b4f23d3e1cb2c86
Guillermo Costilla <gcostilla96@gmail.com>
2025-07-29 21:10:26 -0300

    Protected RQ

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { paymentController } from "../controllers/paymentController.js"
+import { verifyToken } from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
 // Rutas de pago con Stripe
 router.post("/process-payment", paymentController.processPayment)
-router.post("/create-payment-intent", paymentController.createPaymentIntent)
+router.post("/create-payment-intent", verifyToken, paymentController.createPaymentIntent)
 router.post("/confirm-payment", paymentController.confirmPayment)
 router.get("/payment-status/:payment_intent_id", paymentController.getPaymentStatus)
 router.get("/public-key", paymentController.getPublicKey)
